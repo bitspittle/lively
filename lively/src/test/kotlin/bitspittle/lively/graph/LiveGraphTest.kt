366a3f210bdc6b9c975b7efbe0063c557f2f5758
David Herman <bit.spittle@gmail.com>
2019-12-22 20:12:03 -0800

    Add test to verify it doesn't work if an executor spawns a new thread

--- lively/src/test/kotlin/bitspittle/lively/graph/LiveGraphTest.kt
+++ lively/src/test/kotlin/bitspittle/lively/graph/LiveGraphTest.kt
@@ -1,7 +1,10 @@
 package bitspittle.lively.graph
 
+import bitspittle.lively.Lively
+import bitspittle.lively.exec.Executor
 import bitspittle.truthish.assertThat
 import org.junit.Test
+import kotlin.IllegalStateException
 
 class LiveGraphTest {
     @Test
@@ -18,5 +21,37 @@ class LiveGraphTest {
         threads.forEach { thread -> thread.join() }
 
         assertThat(liveGraph1!!).isNotSameAs(liveGraph2!!)
+    }
+
+    @Test
+    fun executorCannotSneakilyRunTasksOnADifferentThread() {
+        var exceptionThrownByExecutorThread = false
+        val sneakyExecutor = object : Executor {
+            override fun submit(runnable: () -> Unit) {
+                Thread {
+                    try {
+                        runnable()
+                    }
+                    catch (e: IllegalStateException) {
+                        exceptionThrownByExecutorThread = true
+                    }
+                }.apply {
+                    start()
+                    join()
+                }
+            }
+        }
+        val liveGraph = LiveGraph(sneakyExecutor)
+        val lively = Lively(liveGraph)
+
+        val liveInt1 = lively.create(123)
+        val liveInt2 = lively.create { liveInt1.get() * 2 }
+
+        assertThat(liveInt2.getSnapshot()).isEqualTo(246)
+
+        assertThat(exceptionThrownByExecutorThread).isFalse()
+        liveInt1.set(200)
+        assertThat(exceptionThrownByExecutorThread).isTrue()
+        assertThat(liveInt2.getSnapshot()).isEqualTo(246) // Invalid executor didn't affect state
     }
 }
